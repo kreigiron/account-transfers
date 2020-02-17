4aac0ad20e7346df7526542b3cac1c57cab8c2e3
Erik Giron <1243595+kreigiron@users.noreply.github.com>
2020-02-16 21:46:34 -0600

    remove redundand pom

---
+++ account-transfers/src/main/resources/db/migration/V1__CreateTransferTables.sql
@@ -0,0 +1,15 @@
+create table account (
+    id long not null,
+    name varchar(256) not null,
+    number varchar(20) not null
+)
+
+create table transfer (
+    id long not null,
+    origin_account long not null,
+    destination_account long not null,
+    amount decimal(10,2) not null,
+    status int not null,
+    status_reason varchar(256) not null
+)
+
